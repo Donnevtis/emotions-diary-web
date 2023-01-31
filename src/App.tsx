03bf89ad5312131fad01aee2a4c67bf949e51c94
Donnevtis <nevtees@gmail.com>
2023-01-31 13:31:20 +0300

    encapsulate button logic

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,24 @@ import React from 'react';
 import Container from '@mui/material/Container';
 import Stack from '@mui/material/Stack';
 import ReportButton from './components/ReportButton/ReportButton';
-import Button from '@mui/material/Button';
 import Box from '@mui/system/Box';
-import { useTranslation } from 'react-i18next';
-import { Link, Outlet } from 'react-router-dom';
+
+import { Outlet } from 'react-router-dom';
 import MoodPicker from './layouts/MoodPicker';
 import './App.css';
-import { PATHS } from './types';
+import SettingsButton from './components/SettingsButton/SettingsButton';
 
-const App = () => {
-  const { t } = useTranslation();
-
-  return (
-    <Container>
-      <Outlet />
-      <Stack justifyContent='space-around' gap={1}>
-        <MoodPicker />
-        <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-          <ReportButton />
-          <Button
-            component={Link}
-            to={PATHS.settings}
-          >{t`buttons:notificationSettings`}</Button>
-        </Box>
-      </Stack>
-    </Container>
-  );
-};
+const App = () => (
+  <Container>
+    <Outlet />
+    <Stack justifyContent='space-around' gap={1}>
+      <MoodPicker />
+      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+        <ReportButton />
+        <SettingsButton />
+      </Box>
+    </Stack>
+  </Container>
+);
 
 export default App;
